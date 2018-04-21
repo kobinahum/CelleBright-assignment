dbc31fd558c6c743e2d4ddafa8eff5ffaf95accb
kobinahum <38466008+kobinahum@users.noreply.github.com>
2018-04-21 10:54:20 +0300

    Update Run.java

--- Run.java
+++ Run.java
@@ -3,13 +3,15 @@ package test;
 public class Run {
 
 	public static void main(String[] args) {
+		
 		//change to yours file path
-		String filePath="C:/Users/KobKob/Desktop/New folder/ex_v5.txt";
+		String filePath="enter file location here";
 		//choose a path to save the images
-		String saveImagePath="c:/Games/";
+		String saveImagePath="enter path to save the images";
 		Decoder decoder=new Decoder(filePath,saveImagePath,new PhoneBook());
 		decoder.decodePhoneBook();
 		decoder.getPhoneBook().printPhoneBook();
+		
 		
 	
 		
